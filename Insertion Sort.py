a74b316a3ec0486e1e7a562151f5fd6055ebb19d
polireddypolireddy94-a11y <polireddypolireddy94@gmail.com>
2026-07-23 16:03:35 +0530

    Create Insertion Sort.py

---
+++ Insertion Sort.py
@@ -0,0 +1,9 @@
+arr = [75, 3, 4, 2, 1]
+n = len(arr)
+for i in range(n - 1):
+    for j in range(n - 1 - i):
+        if arr[j] > arr[j + 1]:
+            temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp
+print("Sorted Array:", arr)
